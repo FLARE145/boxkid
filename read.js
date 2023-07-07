6cae287c14d10361fd5c5303eb881726fb0396b6
thekikoman <kikoknowsyouall@gmail.com>
2023-07-07 01:45:58 -0500

    fullscreen work

--- read.js
+++ read.js
@@ -21,7 +21,7 @@ function getChapter(){
 function getImages(){
 	//gets format setting from cookie
 	setting = "page";
-	if (readCookie('scrollFormat') != 'true'){
+	if (readCookie('viewFormat') === 'page'){
 		setting = "page";
 		document.getElementById("comicScrollView").remove();
 		imageArray = chapters[getChapter()][setting];
@@ -45,7 +45,7 @@ function getImages(){
 
 function setImage(pageNumber){
 	let viewer = document.getElementById("comicPageView");
-	viewer.innerHTML = '<img src="' + imageArray[pageNumber] + '">';
+	viewer.innerHTML = '<img src="' + imageArray[pageNumber] + '" onlclick="toggleFullscreen()">';
 };
 
 function nextPage(){
@@ -63,3 +63,52 @@ function previousPage(){
 	currentPage--;
 	setImage(currentPage);
 };
+
+//fullscreen stuff
+function toggleFullscreen(){
+	let width = screen.width;
+	if (width > 1000){
+		if (document.getElementsByClassName("fullscreenImg").length < 1){
+			newElement = document.createElement("div");
+			newElement.classList.add("fullscreenImg");
+			newElement.setAttribute("onclick","toggleFullscreen();");
+			newElement.innerHTML = '<img src="' + imageArray[currentPage] + '">';
+			
+			// Enter fullscreen mode
+			  const element = document.documentElement;
+			  if (element.requestFullscreen) {
+				element.requestFullscreen();
+			  } else if (element.mozRequestFullScreen) {
+				element.mozRequestFullScreen();
+			  } else if (element.webkitRequestFullscreen) {
+				element.webkitRequestFullscreen();
+			  } else if (element.msRequestFullscreen) {
+				element.msRequestFullscreen();
+			  }
+			
+			document.getElementsByClassName("window")[0].append(newElement);
+		} else{
+			// Exit fullscreen mode
+			  if (document.exitFullscreen) {
+				document.exitFullscreen();
+			  } else if (document.mozCancelFullScreen) {
+				document.mozCancelFullScreen();
+			  } else if (document.webkitExitFullscreen) {
+				document.webkitExitFullscreen();
+			  } else if (document.msExitFullscreen) {
+				document.msExitFullscreen();
+			  }
+			
+			document.getElementsByClassName("fullscreenImg")[0].remove();
+		}
+	}
+	console.log(width);
+};
+
+//escape deletes elm
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Escape') {
+    document.getElementsByClassName("fullscreenImg")[0].remove();
+  }
+});
